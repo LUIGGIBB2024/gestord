1c98bd8d2e8853bd04c9ce7ca7ae5156caeac103
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-11 06:50:37 -0500

    Updated Software

--- app/Http/Controllers/api/UpdateController.php
+++ app/Http/Controllers/api/UpdateController.php
@@ -28,7 +28,7 @@ class UpdateController extends Controller
                {
                 return response()->json(
                     [
-                       'status' => '405',
+                       'status' => '404',
                        'msg'  => 'Imposible la Actualización',
                        'data' =>  "Apertura Clientes OK",
                       ],404);
